379de3c07c717862f7447a7baf38cd000330da33
krkarizs <sevenke@gmail.com>
2021-07-06 10:08:07 +0300

    basic for loop has been added

--- peli.py
+++ peli.py
@@ -3,3 +3,14 @@ quiz = {
     1: {"question": "Who was the leader fo the group?", "answer": "Rick"},
     2: {"question": "Who is Carols best friend?", "answer" : "Deryl"}
 }
+
+score = 0
+
+for question in quiz:
+    print(quiz[question]["question"])
+    answer = input("Enter your answer: ")
+
+    if answer == quiz[question]["answer"]:
+        score += 1
+
+print(score)
