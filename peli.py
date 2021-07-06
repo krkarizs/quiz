e6b33d6e188fc22601214f43c7c01da5c18f3d3f
krkarizs <sevenke@gmail.com>
2021-07-06 10:54:40 +0300

    Game asks if the player wants to play again

--- peli.py
+++ peli.py
@@ -4,13 +4,22 @@ quiz = {
     2: {"question": "Who is Carols best friend?", "answer" : "Deryl"}
 }
 
-score = 0
+while True:
+    score = 0
 
-for question in quiz:
-    print(quiz[question]["question"])
-    answer = input("Enter your answer: ")
+    for question in quiz:
+        print(quiz[question]["question"])
+        answer = input("Enter your answer: ")
 
-    if answer == quiz[question]["answer"]:
-        score += 1
+        if answer == quiz[question]["answer"]:
+            print("Good job! Your answer is correct!")
+            score += 1
+        else:
+            print("Opps! Wrong answer!")
 
-print(score)
+    print(f"Your score is: {score}")
+
+    again = input("Would you like to try again? Write y or n!")
+    if again == "n":
+        print("Thanks for playing with us!")
+        break
